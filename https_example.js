eeeb368ca9fb9cbca681dc3adb1a883a621a800c
MPriest <miche.priest@gmail.com>
2018-09-02 14:26:49 -0700

    edited Step 6: Read the data so that the code would run

--- https_example.js
+++ https_example.js
@@ -8,11 +8,13 @@ var options = {
   };
   
   // called by https when the request is made.
-  var callback = function(response) {
-    console.log('In response handler callback!');
-    
-    response.on('data', function(chunk) {
-        console.log('[-- CHUNK OF LENGTH ' + chunk.length + ' --]');
-        console.log(chunk.toString());
-      });
-    }
+var callback = function(response) {
+  console.log('In response handler callback!');
+
+  response.on('data', function(chunk) {
+      console.log('[-- CHUNK OF LENGTH ' + chunk.length + ' --]');
+      console.log(chunk.toString());
+    });
+  }
+
+  https.request(options, callback).end();
